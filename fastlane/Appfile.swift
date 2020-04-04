7c5768316d965aac67c559df567fbe8f55ed5817
Kevin Sullivan <kevin@sideapps.com>
2020-04-03 20:51:04 -0400

    Install Fastlane

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.sideapps.StalkCentral" } // The bundle identifier of your app
+var appleID: String { return "sideslapd@gmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "459469" } // App Store Connect Team ID
+var teamID: String { return "S7BT87895F" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
